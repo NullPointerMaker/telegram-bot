0f8ecdc269354b6a547225124fa2e7c920fc23d9
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-03-16 21:28:31 +0800

    修复转义错误

--- src/main/java/npDev/telegramBot/Utilities.java
+++ src/main/java/npDev/telegramBot/Utilities.java
@@ -16,16 +16,6 @@ public class Utilities {
     }
 
     /**
-     * 给一段字符串中的<code>string.format()</code>转义字符添加<code>\b</code>标记
-     * @param text 需要标记为不转义成<code>string.format()</code>的字符串
-     * @return 将<code>string.format()</code>转义字符标记为不转义的字符串
-     */
-    public static String prepareNotToStringFormat(String text) {
-        text = text.replace("%", "\b%");
-        return text;
-    }
-
-    /**
      * 如果字符串中有经过<code>prepareNotToMarkdown</code>处理的转义字符，那么它们将会被转回原始字符。
      * @param text 需要转义成markdown友好的字符串
      * @return 转义成markdown友好的字符串
@@ -33,7 +23,6 @@ public class Utilities {
     public static String completeToMarkdown(String text) {
         text = text.replace("_", "\\_").replace("*", "\\*").replace("`", "\\`").replace("[", "\\[");
         text = text.replace("\b\\_", "_").replace("\b\\*", "*").replace("\b\\`", "`").replace("\b\\[", "[");
-        text= text.replace("\bs","%s");
         return text;
     }
 
